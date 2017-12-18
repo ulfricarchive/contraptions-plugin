e7400fbf410e9e7b2a8a0468da17e73c7d3fe9a7
insou22 <zac.kologlu@gmail.com>
2017-12-18 16:28:25 +1100

    Forgot you have to save in Eclipse

--- src/main/java/com/ulfric/plugin/contraptions/elevators/ElevatorSignListener.java
+++ src/main/java/com/ulfric/plugin/contraptions/elevators/ElevatorSignListener.java
@@ -1,18 +1,32 @@
 package com.ulfric.plugin.contraptions.elevators;
 
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
+import com.google.common.base.Strings;
+import com.ulfric.dragoon.extension.inject.Inject;
 import com.ulfric.plugin.locale.TellService;
+import com.ulfric.plugin.tasks.Scheduler;
 
 public class ElevatorSignListener implements Listener {
+	
+	@Inject
+	private ElevatorService elevatorService;
+	
+	@Inject
+	private Scheduler scheduler;
 
 	@EventHandler
 	public void on(BlockPlaceEvent event) {
+		
 		Block block = event.getBlockPlaced();
 		Player player = event.getPlayer();
 		
@@ -32,6 +46,44 @@ public class ElevatorSignListener implements Listener {
 			
 			return;
 		}
+		
+		rewriteSign(sign, direction);
+		elevatorService.createElevator(sign, direction);
+		TellService.sendMessage(player, "elevator-created");
+	}
+	
+	@EventHandler
+	public void on(BlockBreakEvent event) {
+		Block block = event.getBlock();
+		
+		if (block == null || !(block.getState() instanceof Sign)) {
+			return;
+		}
+		
+		elevatorService.deleteElevator(block.getLocation());
+	}
+	
+	@EventHandler
+	public void on(PlayerInteractEvent event) {
+		Block block = event.getClickedBlock();
+		
+		if (block == null || !(block.getState() instanceof Sign)) {
+			return;
+		}
+		
+		elevatorService.getElevator(block.getLocation()).whenComplete((elevator, throwable) -> {
+			if (elevator != null && throwable == null) {
+				scheduler.runOnMainThread(() -> {
+					elevatorService.elevate(elevator, event.getPlayer());
+				});
+			}
+		});
+		
+	}
+	
+	private void rewriteSign(Sign sign, ElevatorDirection direction) {
+		sign.setLine(0,  "[Elevator]");
+		sign.setLine(1, StringUtils.capitalize(direction.name().toLowerCase()));
 	}
 	
 	private boolean isElevatorSign(Block block) {
