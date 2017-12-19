497895a3d1104c7cd0f7e12dd7dafdc96c5b1505
insou22 <zac.kologlu@gmail.com>
2017-12-19 04:22:18 -0500

    Elevator changes

--- src/main/java/com/ulfric/plugin/contraptions/elevators/ElevatorSignListener.java
+++ src/main/java/com/ulfric/plugin/contraptions/elevators/ElevatorSignListener.java
@@ -1,33 +1,24 @@
 package com.ulfric.plugin.contraptions.elevators;
 
 import org.apache.commons.lang3.StringUtils;
-import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
-import com.google.common.base.Strings;
-import com.ulfric.dragoon.extension.inject.Inject;
 import com.ulfric.plugin.locale.TellService;
-import com.ulfric.plugin.tasks.Scheduler;
 
 public class ElevatorSignListener implements Listener {
-	
-	@Inject
-	private ElevatorService elevatorService;
-	
-	@Inject
-	private Scheduler scheduler;
 
 	@EventHandler
-	public void on(BlockPlaceEvent event) {
+	public void on(SignChangeEvent event) {
 		
-		Block block = event.getBlockPlaced();
+		Block block = event.getBlock();
 		Player player = event.getPlayer();
 		
 		if (!isElevatorSign(block)) {
@@ -36,9 +27,12 @@ public class ElevatorSignListener implements Listener {
 		
 		Sign sign = (Sign) block.getState();
 		
-		ElevatorDirection direction = ElevatorDirection.caseInsensitiveDirection(sign.getLine(1));
+		ElevatorDirection direction = ElevatorDirection.caseInsensitiveDirection(event.getLine(1));
 		
 		if (direction == null) {
+
+			event.setCancelled(true);
+			cancelSignPlace(block);
 			
 			if (player != null) {
 				TellService.sendMessage(player, "elevator-invalid-direction");
@@ -48,19 +42,7 @@ public class ElevatorSignListener implements Listener {
 		}
 		
 		rewriteSign(sign, direction);
-		elevatorService.createElevator(sign, direction);
 		TellService.sendMessage(player, "elevator-created");
-	}
-	
-	@EventHandler
-	public void on(BlockBreakEvent event) {
-		Block block = event.getBlock();
-		
-		if (block == null || !(block.getState() instanceof Sign)) {
-			return;
-		}
-		
-		elevatorService.deleteElevator(block.getLocation());
 	}
 	
 	@EventHandler
@@ -71,19 +53,24 @@ public class ElevatorSignListener implements Listener {
 			return;
 		}
 		
-		elevatorService.getElevator(block.getLocation()).whenComplete((elevator, throwable) -> {
-			if (elevator != null && throwable == null) {
-				scheduler.runOnMainThread(() -> {
-					elevatorService.elevate(elevator, event.getPlayer());
-				});
-			}
-		});
-		
+		if (isElevatorSign(block) && !event.getPlayer().isSneaking()) {
+			Sign sign = (Sign) block.getState();
+			ElevatorDirection direction = ElevatorDirection.caseInsensitiveDirection(sign.getLine(1));
+			
+			event.getPlayer().teleport(direction.closestTeleport(sign.getLocation()));
+		}
 	}
 	
 	private void rewriteSign(Sign sign, ElevatorDirection direction) {
 		sign.setLine(0,  "[Elevator]");
 		sign.setLine(1, StringUtils.capitalize(direction.name().toLowerCase()));
+	}
+	
+	private void cancelSignPlace(Block block) {
+		block.setType(Material.AIR);
+		
+		ItemStack item = new ItemStack(Material.SIGN); 
+		block.getLocation().getWorld().dropItemNaturally(block.getLocation(), item);
 	}
 	
 	private boolean isElevatorSign(Block block) {
--- src/main/java/com/ulfric/plugin/contraptions/elevators/ElevatorsContainer.java
+++ src/main/java/com/ulfric/plugin/contraptions/elevators/ElevatorsContainer.java
@@ -1,12 +1,10 @@
 package com.ulfric.plugin.contraptions.elevators;
 
 import com.ulfric.dragoon.application.Container;
-import com.ulfric.plugin.contraptions.elevators.store.PersistentElevators;
 
 public class ElevatorsContainer extends Container {
 
 	public ElevatorsContainer() {
-		install(PersistentElevators.class);
 		install(ElevatorSignListener.class);
 	}
 
