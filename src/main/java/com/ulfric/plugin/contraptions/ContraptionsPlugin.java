5ce686cd27caa134c49bf5a37818d44966fb4bfe
Adam Harrison <ad@mharrison.us>
2017-12-22 00:12:24 +0000

    fix sign agent listener loading

--- src/main/java/com/ulfric/plugin/contraptions/ContraptionsPlugin.java
+++ src/main/java/com/ulfric/plugin/contraptions/ContraptionsPlugin.java
@@ -3,12 +3,14 @@ package com.ulfric.plugin.contraptions;
 import com.ulfric.plugin.Plugin;
 import com.ulfric.plugin.contraptions.elevator.ElevatorAgent;
 import com.ulfric.plugin.contraptions.internal.SignAgentFeature;
+import com.ulfric.plugin.contraptions.internal.SignAgentListener;
 
 public class ContraptionsPlugin extends Plugin {
 
 	public ContraptionsPlugin() {
 		install(SignAgentFeature.class);
 		install(SignAgentService.class);
+		install(SignAgentListener.class);
 		install(ElevatorAgent.class);
 	}
 
